019b589fe6bed5eaa1e98a68e58746c88e4fd919
ghostrider5024 <86966757+ghostrider5024@users.noreply.github.com>
2025-05-22 14:33:53 +0700

    update (model):init ModernMessageBoxStyle to storage Style of ModernMessageBox

---
+++ ModernMessageBoxWPF/Models/ModernMessageBoxStyle.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ModernMessageBoxWPF.Models
+{
+    public class ModernMessageBoxStyle
+    {
+        public Style RootBorderStyle { get; set; } = default!;
+        public Style CancelButtonStyle { get; set; } = default!;
+        public Style ConfirmButtonStyle { get; set; } = default!;
+
+    }
+}

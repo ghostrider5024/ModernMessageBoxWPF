c218a65bf414abf5dca337878cf389fcbc67eb69
ghostrider5024 <86966757+ghostrider5024@users.noreply.github.com>
2025-05-22 14:34:48 +0700

    update (service): add new prop style to ModernMessageBoxManager.ShowDialogAsync setStyle for ModernMessageBoxStyle

--- ModernMessageBoxWPF/Managers/ModernMessageBoxManager.cs
+++ ModernMessageBoxWPF/Managers/ModernMessageBoxManager.cs
@@ -3,16 +3,21 @@
 using ModernMessageBoxWPF.ViewModels;
 using ModernMessageBoxWPF.Enums;
 using ModernMessageBoxWPF.UI;
+using ModernMessageBoxWPF.Models;
 
 namespace ModernMessageBoxWPF.Managers
 {
     public static class ModernMessageBoxManager
     {
         private static ModernMessageBox? dialogHolder;
-        public static async Task<bool> ShowDialogAsync(Window? owner, string title, string message, DialogType type = DialogType.Info)
+        public static async Task<bool> ShowDialogAsync(Window? owner
+            , string title, string message, DialogType type = DialogType.Info
+            , ModernMessageBoxStyle? style = null)
         {
             var vm = new ModernMessageBoxViewModel(title, message, type);
             var dialog = dialogHolder ?? new ModernMessageBox();
+
+            dialog.SetStyle(style);
 
             dialog.Owner = owner;
             dialog.DataContext = vm;

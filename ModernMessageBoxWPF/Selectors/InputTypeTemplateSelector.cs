848b0d7f61ef7677d937b416f7cf727177a8bb29
ghostrider5024 <86966757+ghostrider5024@users.noreply.github.com>
2025-05-23 10:45:20 +0700

    chore (ui): init InputTypeTemplateSelector to gen control for ModernMesageBoxInputType

---
+++ ModernMessageBoxWPF/Selectors/InputTypeTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+using ModernMessageBoxWPF.Enums;
+
+namespace ModernMessageBoxWPF.Selectors
+{
+    public class InputTypeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate NormalTemplate { get; set; }
+        public DataTemplate InputTextTemplate { get; set; }
+        public DataTemplate InputPasswordTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ModernMessageBoxInputTypeEnum type)
+            {
+                return type switch
+                {
+                    ModernMessageBoxInputTypeEnum.Normal => NormalTemplate,
+                    ModernMessageBoxInputTypeEnum.InputText => InputTextTemplate,
+                    ModernMessageBoxInputTypeEnum.InputPassword => InputPasswordTemplate,
+                    _ => NormalTemplate
+                };
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+
+}

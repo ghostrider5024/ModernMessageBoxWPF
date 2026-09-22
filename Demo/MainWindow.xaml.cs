d048146bc5386d41618505b74202fe94e26e20d1
ghostrider5024 <tle516967@gmail.com>
2026-09-22 10:22:42 +0700

    update (ui): replace Grid with StackPanel and enhance button functionality in MainWindow

--- Demo/MainWindow.xaml.cs
+++ Demo/MainWindow.xaml.cs
@@ -17,22 +17,86 @@ namespace Demo
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            var cancelButtonStyle =FindResource("CancelButtonStyle") as Style;
+
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
+            var result = await ModernMessageBoxManager
+                .ShowDialogAsync(this
+                , "Xác nhận"
+                , "Bạn muốn thoát ứng dụng?"
+                , MessageBoxStateType.Info
+                , ModernMessageBoxInputTypeEnum.Normal
+                , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle()
+                {
+                    //CancelButtonStyle = cancelButtonStyle
+                });
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
             var result = await ModernMessageBoxManager
                 .ShowDialogAsync(this
                 , "Xác nhận"
                 , "Bạn muốn thoát ứng dụng?"
                 , MessageBoxStateType.Success
-                , ModernMessageBoxInputTypeEnum.InputText
+                , ModernMessageBoxInputTypeEnum.Normal
                 , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle()
                 {
                     //CancelButtonStyle = cancelButtonStyle
                 });
-            var temp = 2;
-            //if (result)
-            //{
-            //    Application.Current.Shutdown();
-            //}
+            
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
+            var result = await ModernMessageBoxManager
+                .ShowDialogAsync(this
+                , "Xác nhận"
+                , "Bạn muốn thoát ứng dụng?"
+                , MessageBoxStateType.Warning
+                , ModernMessageBoxInputTypeEnum.Normal
+                , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle(),
+               null);
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
+            var result = await ModernMessageBoxManager
+                .ShowDialogAsync(this
+                , "Xác nhận"
+                , "Bạn muốn thoát ứng dụng?"
+                , MessageBoxStateType.Warning
+                , ModernMessageBoxInputTypeEnum.Normal
+                , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle(),
+                 null);
+        }
+
+        private async void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
+            var result = await ModernMessageBoxManager
+                .ShowDialogAsync(this
+                , "Xác nhận"
+                , "Bạn muốn thoát ứng dụng?"
+                , MessageBoxStateType.Warning
+                , ModernMessageBoxInputTypeEnum.InputText
+                , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle(),
+                null);
+        }
+
+        private async void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            var cancelButtonStyle = FindResource("CancelButtonStyle") as Style;
+            var result = await ModernMessageBoxManager
+                .ShowDialogAsync(this
+                , "Xác nhận"
+                , "Bạn muốn thoát ứng dụng?"
+                , MessageBoxStateType.Warning
+                , ModernMessageBoxInputTypeEnum.InputPassword
+                , new ModernMessageBoxWPF.Models.ModernMessageBoxStyle(),
+                null);
         }
     }
 }
